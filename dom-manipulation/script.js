522b49d5cec5df1597a5268ea9e4167bf4553a49
Thabang <phoshokothabang1@gmail.com>
2025-08-19 22:11:16 +0200

    feat: add a function that displays a form to add a new quote

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -6,22 +6,31 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.appendChild(button); // Append it to the body
 });
 
+// Get elements from the DOM
 const quoteDisplay = document.getElementById('quoteDisplay');
 const showQuoteButton = document.getElementById('newQuote');
 
 const quotes = [
     {
         text: "The only limit to our realization of tomorrow is our doubts of today.",
-        category: "inspirational"},
+        category: "inspirational"
+    },
     {
         text: "Life is 10% what happens to us and 90% how we react to it.",
-        category: "motivational"},
+        category: "motivational"
+    },
     { 
-        text: "Stay positive!", category: "Motivation" },
+        text: "Stay positive!", 
+        category: "Motivation" 
+    },
     { 
-        text: "Code is like humor.", category: "Programming" },
+        text: "Code is like humor.", 
+        category: "Programming" 
+    },
     { 
-        text: "Simplicity is the soul of efficiency.", category: "Wisdom" },
+        text: "Simplicity is the soul of efficiency.", 
+        category: "Wisdom" 
+    },
 ];
 
 
@@ -33,7 +42,22 @@ function showRandomQuote() {
 
 //add new quote
 function createAddQuoteForm() {
-    
+
+    quoteDisplay.style.display = 'none'; // Hide the quote display
+    showQuoteButton.style.display = 'none'; // Hide the show quote button
+    document.getElementById("addNewQuote").style.display = "none";
+
+
+    // Create a form to add a new quote
+    const formContainer = document.createElement('div');
+    formContainer.innerHTML = `
+        <h2>Add a New Quote</h2>
+        <input id="newQuoteText" type="text" placeholder="Enter a new quote" /></br>
+        <input id="newQuoteCategory" type="text" placeholder="Enter quote category" /></br>
+        <button onclick="addQuote()">Add Quote</button>
+    `;
+
+    document.body.appendChild(formContainer);
 }
 
 function displayRandomQuote() {
