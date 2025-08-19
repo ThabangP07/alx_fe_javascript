58e473537616b5cd403ff034bdecc7a46a5752e1
Thabang <phoshokothabang1@gmail.com>
2025-08-19 20:32:42 +0200

    feat: add a new button dynamically when the page loads

---
+++ dom-manipulation/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Create a button to add a quote
+    const button = document.createElement('button');
+    button.id = 'newQuote';
+    button.textContent = 'Add New Quote';
+    document.body.appendChild(button); // Append it to the body
+});
+
+//show a random quote
+function showRandomQuote() {
+    pass;
+}
+
+//add new quote
+function createAddQuoteForm() {
+    pass;
+}
+
